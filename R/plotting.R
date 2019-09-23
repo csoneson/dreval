00a5564260df04f8144cc5b00362c67d19028a34
Charlotte Soneson <charlottesoneson@gmail.com>
2019-09-23 14:26:27 +0200

    Allow sorting of bars in rank summary plot, and using only local or global metrics.

--- R/plotting.R
+++ R/plotting.R
@@ -10,7 +10,11 @@
 #'   \code{dreval()}.
 #' @param metrics A character vector with the metrics to include in the summary.
 #'   Must be a subset of the column names of \code{dreSummary}. If \code{NULL},
-#'   all metrics will be used.
+#'   all metrics will be used. It can also be "global" or "local", in which case
+#'   all the global or local metrics, respectively, will be used.
+#' @param sortBars A character scalar indicating whether/how to sort the bars in
+#'   the output. Either "decreasing", "increasing" or "none" (in which case the
+#'   input order will be used).
 #'
 #' @author Charlotte Soneson
 #'
@@ -19,32 +23,36 @@
 #' @export
 #'
 #' @importFrom grDevices colorRampPalette
-#' @importFrom dplyr select mutate vars
+#' @importFrom dplyr select mutate vars group_by ungroup arrange mutate_at desc
 #' @importFrom tidyr gather
 #' @importFrom ggplot2 ggplot aes geom_bar theme_bw scale_fill_manual
 #'
-plotRankSummary <- function(dreSummary, metrics = NULL) {
+plotRankSummary <- function(dreSummary, metrics = NULL,
+                            sortBars = "decreasing") {
     myfun <- function(w) {
         order(order(w))
     }
 
     ## Define metrics to use
     cn <- colnames(dreSummary)
+
+    global <- c("coRankingQglobal", "EuclDistBetweenDists", "KSStatDist",
+                "PearsonCorrDist", "SammonStress", "SpearmanCorrDist")
+    local <- c(grep("Continuity_k", cn, value = TRUE),
+               grep("Trustworthiness_k", cn, value = TRUE),
+               "coRankingQlocal",
+               grep("MeanJaccard_k", cn, value = TRUE))
+
     if (is.null(metrics)) {
         metrics <- cn
+    } else if (length(metrics) == 1 && metrics == "global") {
+        metrics <- intersect(cn, global)
+    } else if (length(metrics) == 1 && metrics == "local") {
+        metrics <- intersect(cn, local)
     }
-    global <- intersect(
-        metrics,
-        c("coRankingQglobal", "EuclDistBetweenDists", "KSStatDist",
-          "PearsonCorrDist", "SammonStress", "SpearmanCorrDist")
-    )
-    local <- intersect(
-        metrics,
-        c(grep("Continuity_k", cn, value = TRUE),
-          grep("Trustworthiness_k", cn, value = TRUE),
-          "coRankingQlocal",
-          grep("MeanJaccard_k", cn, value = TRUE))
-    )
+
+    global <- intersect(metrics, global)
+    local <- intersect(metrics, local)
 
     ## Define colors
     globalcols <- grDevices::colorRampPalette(
@@ -61,7 +69,29 @@ plotRankSummary <- function(dreSummary, metrics = NULL) {
         dplyr::select(c("Method", global, local)) %>%
         dplyr::mutate_at(dplyr::vars(-Method), myfun) %>%
         tidyr::gather(key = "metric", value = "score", -Method) %>%
-        dplyr::mutate(metric = factor(metric, levels = c(global, local)))
+        dplyr::mutate(metric = factor(metric, levels = c(global, local))) %>%
+        dplyr::ungroup()
+
+    if (sortBars == "decreasing") {
+        dreSummary <- dreSummary %>% dplyr::group_by(Method) %>%
+            dplyr::mutate(total = sum(score)) %>%
+            dplyr::ungroup() %>%
+            dplyr::arrange(dplyr::desc(total)) %>%
+            dplyr::mutate(Method = factor(Method, levels = unique(Method))) %>%
+            dplyr::select(-total)
+    } else if (sortBars == "increasing") {
+        dreSummary <- dreSummary %>% dplyr::group_by(Method) %>%
+            dplyr::mutate(total = sum(score)) %>%
+            dplyr::ungroup() %>%
+            dplyr::arrange(total) %>%
+            dplyr::mutate(Method = factor(Method, levels = unique(Method))) %>%
+            dplyr::select(-total)
+    } else if (sortBars == "none") {
+        dreSummary <- dreSummary %>%
+            dplyr::mutate(Method = factor(Method, levels = unique(Method)))
+    } else {
+        stop("'sortBars' must be either 'decreasing', 'increasing' or 'none'.")
+    }
 
     ggplot2::ggplot(dreSummary,
                     ggplot2::aes(x = Method, y = score, fill = metric)) +
