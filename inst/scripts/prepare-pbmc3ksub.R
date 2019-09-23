1b2416eddc07df12db4b9ac665dcd95c58b78f0d
Charlotte Soneson <charlottesoneson@gmail.com>
2019-09-23 14:26:00 +0200

    Expand example data

--- inst/scripts/prepare-pbmc3ksub.R
+++ inst/scripts/prepare-pbmc3ksub.R
@@ -12,6 +12,7 @@ rownames(sce) <- scater::uniquifyFeatureNames(
     ID = rowData(sce)$ENSEMBL_ID,
     names = rowData(sce)$Symbol_TENx
 )
+counts(sce) <- as.matrix(counts(sce))
 sce <- scran::computeSumFactors(sce, min.mean = 0.1)
 sce <- scater::normalize(sce)
 logcounts(sce) <- as.matrix(logcounts(sce))
@@ -21,14 +22,32 @@ fit$trend <- new.trend
 dec <- scran::decomposeVar(fit = fit)
 top.dec <- dec[order(dec$bio, decreasing = TRUE), ]
 keep.genes <- rownames(top.dec[top.dec$bio > 0.01, ])
+
 set.seed(1000)
 sce <- scran::denoisePCA(sce, technical = new.trend,
                          subset.row = keep.genes, BSPARAM = IrlbaParam())
 reducedDims(sce)$PCA_k2 <- reducedDims(sce)$PCA[, 1:2]
+
 set.seed(1000)
-sce <- scater::runTSNE(sce, use_dimred = "PCA", perplexity = 30)
+tmp <- scater::runTSNE(sce, use_dimred = "PCA", perplexity = 30)
+reducedDims(sce)$TSNE_perp30 <- reducedDims(tmp)$TSNE
 set.seed(1000)
-sce <- scater::runUMAP(sce, use_dimred = "PCA")
+tmp <- scater::runTSNE(sce, use_dimred = "PCA", perplexity = 5)
+reducedDims(sce)$TSNE_perp5 <- reducedDims(tmp)$TSNE
+set.seed(1000)
+tmp <- scater::runTSNE(sce, use_dimred = "PCA", perplexity = 15)
+reducedDims(sce)$TSNE_perp15 <- reducedDims(tmp)$TSNE
+set.seed(1000)
+tmp <- scater::runTSNE(sce, use_dimred = "PCA", perplexity = 100)
+reducedDims(sce)$TSNE_perp100 <- reducedDims(tmp)$TSNE
+
+set.seed(1000)
+tmp <- scater::runUMAP(sce, use_dimred = "PCA", n_neighbors = 15)
+reducedDims(sce)$UMAP_nn15 <- reducedDims(tmp)$UMAP
+set.seed(1000)
+tmp <- scater::runUMAP(sce, use_dimred = "PCA", n_neighbors = 30)
+reducedDims(sce)$UMAP_nn30 <- reducedDims(tmp)$UMAP
+
 pbmc3ksub <- sce[keep.genes, ]
 
 ## usethis::use_data(pbmc3ksub)
