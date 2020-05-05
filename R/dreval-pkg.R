2cd3270d91aec8cabb8083ef7c8a53fce5f8be50
Charlotte Soneson <charlottesoneson@gmail.com>
2020-05-05 08:07:54 +0200

    pkg description

--- R/dreval-pkg.R
+++ R/dreval-pkg.R
@@ -1,5 +1,9 @@
 #' dreval
 #'
+#' dreval evaluates and compares multiple reduced dimension
+#' representations, based on how well they retain structure from
+#' the original data set.
+#'
 #' @name dreval-pkg
 #' @docType package
 NULL
